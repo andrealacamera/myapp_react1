65723b9a01a0937cac39510891f8f9d8f7b1a97d
andrealacamera <andrea.lacamera0@gmail.com>
2022-07-14 10:47:15 +0200

    Added pages and components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-gray-800 text-gray-200 fixed bottom-0 left-0 w-full h-16'>
+      Footer
+    </footer>
   )
 }
 
