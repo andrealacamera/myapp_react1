18ba9315a938bc6d2390ab1bb545b8cdd5b10d7e
andrealacamera <andrea.lacamera0@gmail.com>
2022-07-14 17:39:23 +0200

    implemented Context and Protected Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+  const Y = new Date().getFullYear();
   return (
-    <footer className='bg-gray-800 text-gray-200 fixed bottom-0 left-0 w-full h-16'>
-      Footer
+    <footer className='bg-gray-800 text-gray-200 fixed bottom-0 left-0 w-full  p-4'>
+      Copyright &copy; {Y} | Andrea La Camera
     </footer>
   )
 }
